816def5ad3fd73470d92717202e27ff3226247d7
Mauro <mauro-507@hotmail.com>
2022-05-03 01:45:22 -0500

    topics prototipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { WebcamImage } from 'ngx-webcam';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,94 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'topicosFinal';
+
+  constructor(private http: HttpClient) { }
+
+  cloudAPI = '<gateway URL>';
+
+  userImage = '';
+  showForm = false;
+  showResult = false;
+  showSimilarity = false;
+  fileName = '';
+  responses3 = {} as any;
+  s3Message = {} as any;
+  passOrnot = '../assets/notOK.jpeg';
+  s3Operation = 0;
+  similarity = 0;
+
+  webcamImage: WebcamImage | undefined;
+ 
+  handleImage(webcamImage: WebcamImage) {
+    this.webcamImage = webcamImage;
+  }
+
+  saveFaceToCollection() {
+    this.showResult = false;
+    this.showForm = true;
+  }
+
+  searchOnCollection(webcamImage: WebcamImage) {
+    this.showForm = false;
+
+    this.s3Operation = 2;
+
+    // request paylaod to save on rekognition collection
+    var requestPayload = JSON.stringify({
+      'facePhoto': webcamImage.imageAsBase64,
+      'operation': this.s3Operation
+    });
+    
+    // http POST call
+    this.makePost('/s3', requestPayload)
+      .subscribe(data => {
+        console.log(data);
+        this.s3Message = data;
+        this.showResult = true;
+
+        var rekData = this.s3Message.body.rek;
+        console.log(rekData);
+        var rekKeys = Object.keys(this.s3Message.body.rek).length;
+        var similarity_percentage = rekKeys != 0 ? rekData['Face']['Confidence'] : 0;
+        this.similarity = similarity_percentage.toFixed(2);
+
+        this.passOrnot = this.s3Message.statusCode === 200 && rekKeys != 0 && similarity_percentage > 70? '../assets/checkOK.jpeg' : '../assets/notOK.jpeg';
+        this.showSimilarity = true;
+      });
+  }
+
+  submit(webcamImage: WebcamImage, form:any){
+    this.fileName = (form.firstName + form.lastName).replace(/ /g, '').toLowerCase();
+
+    console.log(this.fileName + '.jpg');
+    this.s3Operation = 1;
+
+    // request paylaod to save on rekognition collection
+    var requestPayload = JSON.stringify({
+      'facePhoto': webcamImage.imageAsBase64,
+      'fileName': this.fileName,
+      'operation': this.s3Operation
+    });
+
+    // http POST call
+    this.makePost('/s3', requestPayload)
+      .subscribe(data => {
+        console.log(data);
+        this.s3Message = data;
+        this.showResult = true;
+        this.showSimilarity = false;
+
+        this.passOrnot = this.s3Message.statusCode === 200 ? '../assets/checkOK.jpeg' : '../assets/notOK.jpeg';
+      });
+
+    this.showForm = false;
+  }
+
+  refrescar() {
+    window.location.reload();
+  }
+
+  makePost(endpoint: String, payload: any) {
+    return this.http.post(this.cloudAPI + endpoint, payload);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CameraComponent } from './camera/camera.component';
+import { WebcamModule } from 'ngx-webcam';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CameraComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    WebcamModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
